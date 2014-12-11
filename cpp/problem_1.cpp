e6335521a20fb87c58375f7e3b6288f6ec6cb3e4
Philip Wales <ptwales@gmail.com>
2014-12-11 15:42:27 -0600

    Create problem_1.cpp

---
+++ cpp/problem_1.cpp
@@ -0,0 +1,10 @@
+const int LIMIT = 1000;
+
+int main(void) {
+    int sum = 0;
+    for(int i = 1; i < LIMIT; ++i) 
+        if(i % 3 == 0 || i % 5 == 0)
+     		sum += i;
+    printf("%i", sum);
+    return 0;
+}
