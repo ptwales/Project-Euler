1b8ee0edd335da7d5c6900b7249b91b7fa006a54
Philip Wales <ptwales@gmail.com>
2014-12-16 08:44:46 -0600

    +\s

--- python/problem_1.py
+++ python/problem_1.py
@@ -1,10 +1,8 @@
-LIMIT = 1000
-
-def valid(n):
-   return not ((x%5) and (x%3))
+args = (1000)
 
 def problem_1(u):
+    
+    def valid(n):
+        return not ((x%5) and (x%3))
+    
     return sum((n for n in xrange(1, u) if valid(n)))
-
-if __name__ == '__main__':
-    print(problem_1(LIMIT))
