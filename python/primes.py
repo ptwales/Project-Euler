b4c6243fc1c60e4f30d47a00c781f8f0fa0d88cb
Philip Wales <ptwales@gmail.com>
2014-12-17 16:27:49 -0600

    Update primes.py

--- python/primes.py
+++ python/primes.py
@@ -1,5 +1,14 @@
 import itertools
+import math
 
+"""
+>>> fermats_little(2)
+True
+>>> fermats_little(16)
+False
+>>> fermats_little(61)
+True
+"""
 def fermats_little(n):
     for a in xrange(2, n):
         if (a**n) % n != a % n:
@@ -7,26 +16,34 @@ def fermats_little(n):
     return True
 
 
+"""
+>>> naiive_is_prime(2)
+True
+>>> naiive_is_prime(16)
+False
+>>> naiive_is_prime(61)
+True
+"""
 def naiive_is_prime(n):
     
     def smallest_divisor(n):
-        i = 0
-       while True:
-            if i**2 > n:
-                return n
-            elif n % i == 0:
+        for i in xrange(2, int(math.ceil(n**0.5))):
+            if n % i == 0:
                 return i
-            else
-                i += 1
-                
+        return n
+
     return (smallest_divisor(n) == n)
 
-
+"""
+"""
 def infinite_primes(is_prime):
     for n in itertools.count(2):
         if is_prime(n):
             yield n
  
-
+"""
+>>> index_generator(itertools.count(), 42)
+42
+"""
 def index_genertor(gen, i):
      return itertools.islice(gen, i-1, i).next()
