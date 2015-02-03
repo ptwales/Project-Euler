3e76166d4bda859cf4e5d354c8b1ef19b5a27dd9
Philip Wales <ptwales@gmail.com>
2015-02-03 13:40:47 -0600

    Update problem6.scala

--- Scala/problem6.scala
+++ Scala/problem6.scala
@@ -1,10 +1,23 @@
 object Main extends App {
 
-  def offset(method: Int => Int): Int = {
-    (lo: Int, hi: Int) => method(hi) - method(lo - 1)
+  def offset[T](method: T => T)(lo: T = 1, hi: T)(implicit int: Integral[T]): T = {
+    import int._
+    method(hi) - method(lo - 1)
   }
 
-  def gaussSum(n: Int): Int = n*(n + 1)/2
-  def gaussSqrSum(n: Int): Int = n*(2*n+1)*(n+1)/6
+  def gaussSum[T](n: T)(implicit int: Integral[T]): T = {
+    import int._
+    n*(n + 1)/2
+  }
+  
+  def gaussSqrSum[T](n: T)(implicit int: Integral[T]): T = {
+    import int._
+    n*(2*n + 1)*(n + 1)/6
+  }
+  
+  def problem6[T](n: T)(implicit int: Integral[T]): T = {
+    val gsum = gaussSum(n)
+    (gsum * gsum) - gaussSqrSum(n)
+  }
   
 }
