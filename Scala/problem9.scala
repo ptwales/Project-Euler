5497efb9b8bc7b5615c5a8170ebe42c975222871
Philip Wales <ptwales@gmail.com>
2015-02-04 14:58:40 -0600

    imperfect

---
+++ Scala/problem9.scala
@@ -0,0 +1,25 @@
+object Main extends App {
+
+  def sqr(y: Double): Double = {
+    scala.math.pow(y, 2)
+  }
+	
+	def problem9(x: Int): Option[Int] = {
+	
+	  val sqrx = sqr(x) // let in
+	  def fb(a: Double): Double = (sqrx - 2*x*a)/(2*(x-a))
+	  def fc(a: Double, b: Double): Double = scala.math.sqrt(sqr(a) + sqr(b))
+	  
+	  // use streams with filters and find instead
+	  val solution = (for {
+	    a <- (1 to x/3)
+	    val b: Double = fb(a.toDouble)
+	    if b.isWhole
+	    val c: Double = fc(a.toDouble, b.toDouble)
+	    if c.isWhole
+	  } yield (a * b * c).toInt ).headOption
+	  
+	  solution
+	  
+	}
+}
