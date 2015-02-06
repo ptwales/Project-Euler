34a924491ee3c994ed9b0497f625b24721e037c9
Philip Wales <ptwales@gmail.com>
2015-02-06 15:33:22 -0600

    untested

--- Scala/problem14.scala
+++ Scala/problem14.scala
@@ -3,23 +3,51 @@ def isEven(n: Int): Boolean = {
   (n % 2) == 0
 }
 
-def isOdd(n: Int): Boolean = !isEven(n)
-
-def collatz(n: Int): Stream[Int] = {
+type CollatzSeq = Stream[Int]
+def collatz(n: Int): CollatzSeq = {
   
   def collatzNext(n: Int) = {
     if (isEven(n)) n/2
     else 3*n + 1
   }
   
-  n #:: collatz(collatzNext(n))
+  if (n > 1) n #:: collatz(collatzNext(n))
+  else Stream[Int]()
 }
 
 def problem14(limit: Int): Int = {
-
-  val domain: Stream[Int] = (1 until limit).toStream
-  val indexedCollatz = domain map {x => (x, collatz(x))}
-  val indexedLengths = indexedCollatz map {t => (t._1, t._2.length)}
   
-  (indexedLengths maxBy _._2)._1
+  type Answers = Map[Int, Int]
+
+  /*
+   * Calculates the length of a collatz sequence given
+   * a map of known solutions
+   */ 
+  def collatzLength(cs: CollatzSeq, sols: Answers): Int = {
+    
+    def isSolved(n: Int): Boolean = sols.contains(n)
+    lazy val solvedTilLength: Int = cs.indexWhere(isSolved) + 1
+    
+    if (solvedTilLength == 0) cs.length // sequence is already solved
+    else {
+      lazy val lastSolvedVal: Int = cs.take(solvedTilLength).last
+      solvedTilLength + sols(lastSolvedVal)
+    }
+  }
+  
+  def addSolution(n: Int, sols: Answers): Answers = {
+    lazy val len: Int = collatzLength(collatz(n), sols)
+    sols + (n -> len)
+  }
+  
+  def buildSolution(n: Int, sols: Answers): Answers = {
+    if (n == limit) sols
+    else {
+      lazy val newSols: Answers = addSolution(n, sols)
+      buildSolution(n+1, newSols)
+    }
+  }
+  
+  lazy val solution: Answers =  buildSolution(1, Map[Int, Int]())
+  solution.maxBy(_._2)._1
 }
