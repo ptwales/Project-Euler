570f58558b2c6c8e23c1e103f1216bab201d6bbb
Philip Wales <ptwales@gmail.com>
2015-02-06 15:05:07 -0600

    enforcing style

--- Scala/problem01.scala
+++ Scala/problem01.scala
@@ -1,11 +1,14 @@
 def problem1(limit: Int, factors: List[Int]) = {
   
   def cond(n: Int): Boolean = {
-  	factors exists {n % _ == 0}
+    factors.exists(x => n % x == 0})
   }
-  
-  def allInts: Stream[Int] = (1 until limit).toStream
-  def onlyMults: Stream[Int] = allInts filter cond
+  def allInts: Stream[Int] = {
+    1.until(limit).toStream
+  }
+  def onlyMults: Stream[Int] = {
+    allInts.filter(cond)
+  }
   
   onlyMults.sum
 }
